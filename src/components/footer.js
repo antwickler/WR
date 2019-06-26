7b3ae32e7922d40e7674404a3e7e0ed064fbf8c1
antwickler <weerasak.ratchawangmueang@gmail.com>
2019-06-26 15:18:17 +0700

    Modify footer

---
+++ src/components/footer.js
@@ -0,0 +1,117 @@
+import React from "react"
+import styled from "styled-components";
+
+const FooterContainer = styled.footer`
+    background: #F4F6F7;
+    text-align: center;
+    padding: 0.45rem 0rem;
+`;
+
+const Copyright = styled.h5`
+    font-family: Nunito;
+    font-weight: 400;
+    font-size: 18px;
+    margin: 32px auto;
+`;
+
+const SocialIcons = styled.div`
+    width: 280px;
+    margin: 38px auto 10px;
+    padding-bottom: 0;
+    align-self: flex-end;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+const IconLink = styled.a`
+    text-decoration: none;
+    outline: 0;
+    @media (min-width: 420px) {
+        width: 320px;
+    }
+`;
+
+const Icon = styled.img`
+    height: 30px;
+    width: auto;
+    transition: 0.23s;
+    &:hover {
+        transform: translate(0px, -6px);
+    }
+`;
+
+const linkStyles = {
+    height: "33px",
+    width: "33px",
+    padding: "0",
+    margin: "0"
+};
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <FooterContainer>
+                <SocialIcons>
+                    <IconLink
+                        style={linkStyles}
+                        href="https://github.com/antwickler"
+                        target="_blank"
+                    >
+                        <Icon
+                            style={linkStyles}
+                            src={require("../images/icons/github-logo-black.svg")}
+                            alt="Github"
+                        />
+                    </IconLink>
+                    <IconLink
+                        style={linkStyles}
+                        href="https://www.linkedin.com/in/antwickler/"
+                        target="_blank"
+                    >
+                        <Icon
+                            style={linkStyles}
+                            src={
+                                require("../images/icons/linkedin-logo-black.svg")
+                            }
+                            alt="Linkedin"
+                        />
+                    </IconLink>
+                    <IconLink
+                        style={linkStyles}
+                        href="https://www.facebook.com/w.ratchawangmueang/"
+                        target="_blank"
+                    >
+                        <Icon
+                            style={linkStyles}
+                            src={require("../images/icons/facebook-letter-logo-black.svg")}
+                            alt="Facebook"
+                        />
+                    </IconLink>
+                    <IconLink
+                        style={linkStyles}
+                        href="https://www.instagram.com/antwickler/"
+                        target="_blank"
+                    >
+                        <Icon
+                            style={linkStyles}
+                            src={require("../images/icons/instagram-logo-black.svg")}
+                            alt="Intstagram"
+                        />
+                    </IconLink>
+                </SocialIcons>
+                <Copyright
+                    style={
+                        this.props.backgroundIsBlack
+                            ? { color: "white" }
+                            : { color: "black" }
+                    }
+                >
+                    © Weerasak Ratchawangmueang – {new Date().getFullYear()}
+                </Copyright>
+            </FooterContainer>
+        );
+    }
+}
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mi velit, imperdiet quis mi et, dignissim molestie sem. Etiam mollis tortor ut mauris tristique, eget viverra nunc interdum. Proin at leo vel tellus volutpat congue elementum quis enim. Etiam interdum, justo a mattis facilisis, dui odio dignissim turpis, quis interdum elit ex et diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis sodales augue. Vestibulum pretium arcu in nulla elementum, sit amet accumsan neque venenatis. Nullam sollicitudin turpis arcu, a porta est molestie eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi lectus tellus, pretium eu aliquet eget, cursus sed odio. Nunc id pretium dolor, vel tempus leo. Sed sed erat id nibh ornare bibendum sit amet ut quam. Maecenas lobortis nec erat quis dictum. Fusce interdum non urna faucibus pretium. Sed eu urna eget ipsum aliquam tempor at quis sem.</p>
+    <p>Fusce aliquet mauris odio, non cursus lorem finibus nec. Pellentesque bibendum sem id est sagittis, sit amet posuere nulla auctor. Aenean sed lacus sit amet lorem gravida rhoncus. Donec erat elit, fringilla ut auctor at, maximus in metus. Quisque condimentum massa sed libero scelerisque vestibulum. Nullam et maximus tellus. Duis ut auctor ipsum. Nunc auctor, neque eget efficitur dictum, purus arcu vestibulum augue, in efficitur urna ligula vitae orci. Integer varius turpis eu nulla pulvinar feugiat. Suspendisse potenti. Cras quis urna tempus, bibendum ligula sit amet, volutpat ligula. Sed et diam quis purus vulputate sollicitudin. In placerat sem quis ullamcorper condimentum. Donec pulvinar mauris id justo aliquet feugiat. Vestibulum tempus, enim et pharetra fermentum, velit metus rhoncus est, a bibendum enim felis id lectus.</p>
+    <p>Maecenas hendrerit semper enim, in congue massa porttitor eget. Maecenas at blandit metus, vitae varius enim. Morbi semper tellus ex, ac sagittis sem iaculis et. Sed ac rutrum risus. Ut ut est magna. Etiam vitae magna porta, pellentesque neque id, convallis nunc. Vivamus in ornare felis. Donec pellentesque viverra orci, nec rhoncus magna porttitor eget. Sed pretium sem in accumsan laoreet. Curabitur non ipsum enim. Pellentesque congue, sapien at vestibulum pulvinar, urna urna ultrices sem, a euismod eros arcu vel dui. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget urna et tortor hendrerit lacinia. Vivamus gravida erat et orci hendrerit bibendum. Donec bibendum pellentesque dolor non vulputate.</p>
   </Layout>
 )
 
